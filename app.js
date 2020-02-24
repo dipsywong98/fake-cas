4e7b725997fc0fa1df4e4609e645b2687137d4e5
dipsywong98 <ycwongal@connect.ust.hk>
2020-02-24 22:47:11 +0800

    update

--- app.js
+++ app.js
@@ -64,8 +64,8 @@ app.use('/cas/logout', function (req, res) {
 
 app.post('/cas/samlValidate*', function (req, res) {
   let cookie = req.cookies['fake-cas']
-  console.log('samlValidate', req.method, req.originalUrl, req.query, req.body['soap-env:envelope']['soap-env:body'][0]['samlp:request']['samlp:assertion-artifact'])
-  const ticket = req.body['soap-env:envelope']['soap-env:body'][0]['samlp:request']['samlp:assertion-artifact']
+  console.log('samlValidate', req.method, req.originalUrl, req.query, req.body['soap-env:envelope']['soap-env:body'][0]['samlp:request'])
+  const ticket = req.body['soap-env:envelope']['soap-env:body'][0]['samlp:request']
   if (tickets[ticket]) {
     console.log('validate success', tickets[ticket])
     res.send(`
@@ -76,7 +76,7 @@ app.post('/cas/samlValidate*', function (req, res) {
     delete tickets[req.query.ticket]
   } else {
     console.log('validate fail')
-    res.send(`no`)
+    res.send(`<Envelope>no</Envelope>`)
   }
   res.end()
 })
